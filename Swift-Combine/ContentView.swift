0c681a70b447c1616793f57bc70d1710b14d8692
Kyung Hoon Kim <tayesymbol@gmail.com>
2021-11-02 22:33:13 +0900

    Initial Commit

---
+++ Swift-Combine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift-Combine
+//
+//  Created by k2hoon on 2021/11/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift-Combine/Swift_CombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_CombineApp.swift
+//  Swift-Combine
+//
+//  Created by k2hoon on 2021/11/02.
+//
+
+import SwiftUI
+
+@main
+struct Swift_CombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
